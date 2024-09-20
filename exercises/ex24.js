d0afff99bb2f84f11823d45b6caca9ee88e33cce
Chole Seoyeong Ro <nostopzero@gmail.com>
2024-09-20 11:45:14 -0700

    exercise 24 completed

--- exercises/ex24.js
+++ exercises/ex24.js
@@ -20,3 +20,28 @@ console.log(lastIndexOf([ 5, 5, 5 ], 5), "=?", 2);
 console.log(lastIndexOf([], 3), "=?", -1);
 
 */
+
+// function lastIndexOf(arr, param) {
+//   let i = "";
+//   for (let l = 0; l < arr.length; l++) {
+//     if (arr[l] === param) {
+//       i = l;
+//     }
+//   }
+
+//   if (i !== "") {
+//     return i;
+//   } else {
+//     return -1;
+//   }
+// }
+
+function lastIndexOf (arr, num) {
+  return arr.lastIndexOf(num);
+}
+
+console.log(lastIndexOf([ 0, 1, 4, 1, 2 ], 1), "=?", 3);
+console.log(lastIndexOf([ 0, 1, 4, 1, 2 ], 2), "=?", 4);
+console.log(lastIndexOf([ 0, 1, 4, 1, 2 ], 3), "=?", -1);
+console.log(lastIndexOf([ 5, 5, 5 ], 5), "=?", 2);
+console.log(lastIndexOf([], 3), "=?", -1);
